88bd8787ff6a7da7912ac6ae29987491d5dadccc
Ari Oppenheimer <oppenheimer@berkeley.edu>
2017-08-25 15:59:14 -0700

    calculated proportional to footage

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Rent Calculation Utility for Scenic House
 
 Our total square footage is `1626`, and our total rent is $`8800`, putting us at ~`5.41` a square foot.
+
+If we calculate rent as proportional to square footage, using the formula `total_rent * room_n_footage / (total_sq_footage - common_space)`, we get this breakdown:
+
+| room | rent |
+| --- | --- | 
+| daniel: | 473.99965862817294 |
+| grady: | 1489.5905976445347 |
+| francis: | 799.5123259613275 |
+| kim/aziz: | 1206.9932457145644 (603.4966228572822) |
+| maya/dan/seb: | 2823.3986003755094 (941.1328667918365 each)|
+| kelly: | 1206.9932457145644 |
+| ari: | 799.5123259613275 |
--- rent_calc.py
+++ rent_calc.py
@@ -4,27 +4,35 @@
 TOTAL_RENT = 8800
 TOTAL_SQUARE_FOOTAGE = 1626 # $5.41 a square foot
 
-daniel = (7 + 10/12)**2 # perfect closet space, fantastic light, roof deck, bay view
-grady = 13*(14 + 10/12) # patio access, not great light, loud potentially
-francis = 11.5*9 # decent closet space, great light
-kim_aziz = 12.5**2
-maya_dan_seb = 21.5*17 # great light, slightly sub-decent closet space, bay view, sink, shared
-kelley = 12.5**2 # ample closet space, great light, bay view
-ari = 11.5*9 # decent closet space, not much light, sink
+daniel = ("daniel", (7 + 10/12)**2) # perfect closet space, fantastic light, roof deck, bay view
+grady = ("grady", 13*(14 + 10/12)) # patio access, not great light, loud potentially
+francis = ("francis", 11.5*9) # decent closet space, great light
+kim_aziz = ("kim/aziz", 12.5**2) # great light, great closet space, bay view, shared
+maya_dan_seb = ("maya/dan/seb", 21.5*17) # great light, slightly sub-decent closet space, bay view, sink, shared
+kelly = ("kelly", 12.5**2) # ample closet space, great light, bay view
+ari = ("ari", 11.5*9) # decent closet space, not much light, sink
 
-rooms = [daniel, grady, francis, kim_aziz, maya_dan_seb, kelley, ari] 
+rooms = [daniel, grady, francis, kim_aziz, maya_dan_seb, kelly, ari] 
 
-def get_shared_space():
-    return TOTAL_SQUARE_FOOTAGE - sum(rooms)
+def get_common_space():
+    return TOTAL_SQUARE_FOOTAGE - sum([room[1] for room in rooms])
+
+def get_prices():
+    COMMON_SPACE = get_shared_space()
+    for pair in zip([room[0] for room in rooms], map(lambda x: TOTAL_RENT * x[1] / (TOTAL_SQUARE_FOOTAGE - COMMON_SPACE), rooms)):
+        print("{}: {}".format(pair[0], pair[1]))
 
 def add_closet(n):
-    for room in [daniel, kim_aziz, kelley, ari, francis]:
+    for room in [daniel, kim_aziz, kelly, ari, francis]:
         room *= n
 
 def add_natural_light(n):
-    for room in [kim_aziz, maya_dan_seb, kelley]:
+    for room in [kim_aziz, maya_dan_seb, kelly]:
         room *= n
 
 def reduce_shared(n):
     maya_dan_seb *= n/2
     kim_aziz *= n
+
+if __name__ == "__main__":
+    get_prices()
