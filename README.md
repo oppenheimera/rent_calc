06e8b5ae125666e181c919aa24f536e3b949fc10
Ari Oppenheimer <oppenheimer@berkeley.edu>
2017-08-25 17:05:37 -0700

    strugs

--- README.md
+++ README.md
@@ -2,14 +2,30 @@
 
 Our total square footage is `1626`, and our total rent is $`8800`, putting us at ~`5.41` a square foot.
 
-If we calculate rent as proportional to square footage, using the formula `total_rent * room_n_footage / (total_sq_footage - common_space)`, we get this breakdown:
+If we calculate rent as proportional to square footage, using the formula: 
+`total_rent * (room_n_footage / (total_sq_footage - common_space))`, we get this breakdown:
 
 | room | rent |
 | --- | --- | 
 | daniel: | 473.99965862817294 |
 | grady: | 1489.5905976445347 |
 | francis: | 799.5123259613275 |
-| kim/aziz: | 1206.9932457145644 (603.4966228572822) |
+| kim/aziz: | 1206.9932457145644 (603.4966228572822 each) |
 | maya/dan/seb: | 2823.3986003755094 (941.1328667918365 each)|
 | kelly: | 1206.9932457145644 |
 | ari: | 799.5123259613275 |
+
+This some obvious caveats. Rent for the triple is the 2nd most expensive, and rent for Danny G's room (which everyone seemed to want) is the least expensive. In a sentence, this method assumes that square footage is the only priority, when in reality we care about closets, natural light, shared space, views, aesthetics, and random bonuses (like sinks, patio access, or a private roof). 
+
+So try and fix this, I rated rooms on these categories:
+
+| room | closets | :sunny: | shared | :sunrise: | :cool: |
+| daniel | 7/10 | 10/10 | 10/10 | 10/10 | 10/10 |
+| grady | 2/10 | 2/10 | 10/10 | 3/10 | 6/10 |
+| francis | 6/10 | 7/10 | 10/10 | 5/10 |  2/10 |
+| kim/aziz | 8/10 | 10/10 | 5/10 | 10/10 | 0/10 |
+| maya/dan/seb | 3/10 | 10/10| 0/10 | 9/10 | 0/10 |
+| kelly | 10/10 | 10/10 | 10/10 | 10/10 | 8/10 |
+| ari | 7/10 | 2/10 | 10/10 | 2/10 | 1/10 |
+
+These category scores are then transformed into multipliers using the rule: 
--- rent_calc.py
+++ rent_calc.py
@@ -3,14 +3,15 @@
 ###################
 TOTAL_RENT = 8800
 TOTAL_SQUARE_FOOTAGE = 1626 # $5.41 a square foot
+ALPHA = 1.5
 
-daniel = ("daniel", (7 + 10/12)**2) # perfect closet space, fantastic light, roof deck, bay view
-grady = ("grady", 13*(14 + 10/12)) # patio access, not great light, loud potentially
-francis = ("francis", 11.5*9) # decent closet space, great light
-kim_aziz = ("kim/aziz", 12.5**2) # great light, great closet space, bay view, shared
-maya_dan_seb = ("maya/dan/seb", 21.5*17) # great light, slightly sub-decent closet space, bay view, sink, shared
-kelly = ("kelly", 12.5**2) # ample closet space, great light, bay view
-ari = ("ari", 11.5*9) # decent closet space, not much light, sink
+daniel = ["daniel", (7 + 10/12)**2] # perfect closet space, fantastic light, roof deck, bay view
+grady = ["grady", 13*(14 + 10/12)] # patio access, not great light, loud potentially
+francis = ["francis", 11.5*9] # decent closet space, great light
+kim_aziz = ["kim/aziz", 12.5**2] # great light, great closet space, bay view, shared
+maya_dan_seb = ["maya/dan/seb", 21.5*17] # great light, slightly sub-decent closet space, bay view, sink, shared
+kelly = ["kelly", 12.5**2] # ample closet space, great light, bay view
+ari = ["ari", 11.5*9] # decent closet space, not much light, sink
 
 rooms = [daniel, grady, francis, kim_aziz, maya_dan_seb, kelly, ari] 
 
@@ -18,21 +19,59 @@ def get_common_space():
     return TOTAL_SQUARE_FOOTAGE - sum([room[1] for room in rooms])
 
 def get_prices():
-    COMMON_SPACE = get_shared_space()
-    for pair in zip([room[0] for room in rooms], map(lambda x: TOTAL_RENT * x[1] / (TOTAL_SQUARE_FOOTAGE - COMMON_SPACE), rooms)):
+    COMMON_SPACE = get_common_space()
+    s = 0
+    for pair in zip([room[0] for room in rooms], map(lambda x: TOTAL_RENT * (x[1] / (TOTAL_SQUARE_FOOTAGE - COMMON_SPACE)), rooms)):
+        s += pair[1]
         print("{}: {}".format(pair[0], pair[1]))
+    print("total: {}".format(s))
 
-def add_closet(n):
-    for room in [daniel, kim_aziz, kelly, ari, francis]:
-        room *= n
+def add_closet():
+    daniel[1] *= 7/10 * ALPHA
+    grady[1] *= 3.5/10 * ALPHA
+    francis[1] *= 6/10 * ALPHA
+    kim_aziz[1] *= 5/10 * ALPHA
+    maya_dan_seb[1] *= 4/10 * ALPHA
+    kelly[1] *= 9/10 * ALPHA
+    ari[1] *= 7/10 * ALPHA
+ 
+def add_natural_light():
+    daniel[1] *= 10/10 * ALPHA
+    grady[1] *= 4/10 * ALPHA
+    francis[1] *= 7/10 * ALPHA
+    kim_aziz[1] *= 10/10 * ALPHA
+    maya_dan_seb[1] *= 10/10 * ALPHA
+    kelly[1] *= 10/10 * ALPHA
+    ari[1] *= 3/10 * ALPHA
 
-def add_natural_light(n):
-    for room in [kim_aziz, maya_dan_seb, kelly]:
-        room *= n
+def reduce_shared():
+    kim_aziz[1] *=  5/10 * ALPHA
+    maya_dan_seb[1] *= 5/10 * ALPHA
 
-def reduce_shared(n):
-    maya_dan_seb *= n/2
-    kim_aziz *= n
+def add_views():
+    daniel[1] *= 10/10 * ALPHA
+    grady[1] *= 4/10 * ALPHA
+    francis[1] *= 5/10 * ALPHA
+    kim_aziz[1] *= 10/10 * ALPHA
+    maya_dan_seb[1] *= 10/10 * ALPHA
+    kelly[1] *= 10/10 * ALPHA
+    ari[1] *= 3/10 * ALPHA
+
+def add_bonus():
+    daniel[1] *= 10/10 * ALPHA
+    grady[1] *= 6/10 * ALPHA
+    francis[1] *= 2/10 * ALPHA
+    kim_aziz[1] *= 0/10 * ALPHA
+    maya_dan_seb[1] *= 0/10 * ALPHA
+    kelly[1] *= 5/10 * ALPHA
+    ari[1] *= 1/10 * ALPHA
+
+
 
 if __name__ == "__main__":
+    add_closet()
+    add_natural_light()
+    reduce_shared()
+    # add_views()
+    # add_bonus()
     get_prices()
